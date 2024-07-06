fd3e359cb5b96cb1eb20e52f43dd3974a5f96142
Ekaterina <katanatroll@yandex.ru>
2024-07-06 11:21:41 +0300

    register

--- src/pages/Reg.jsx
+++ src/pages/Reg.jsx
@@ -1,11 +1,69 @@
-const Reg = () => (
-    <>
-     
+import React, { useState } from 'react';
+
+const Reg = () => {
+    const [formData, setFormData] = useState({});
+    const [userFound, setUserFound] = useState(false); // добавляем состояние userFound
+
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = `https://apimet.1lop.ru/register?firstname=${formData.firstName}&lastname=${formData.lastName}&email=${formData.email}&password=${formData.password}`;
+    
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+    
+            if (!response.ok) {
+                throw new Error('Ошибка HTTP: ' + response.status);
+            }
+    
+            const contentType = response.headers.get('content-type');
+            if (contentType && contentType.includes('application/json')) {
+                const data = await response.json();
+                setUserFound(true);
+            } else {
+                throw new Error('Неверный формат данных');
+            }
+        } catch (error) {
+            console.error('Ошибка проверки пользователя:', error);
+        }
+    };
+
+    return (
         <div>
-            <h1>Регистрация</h1>
+            <h1>Вход</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    FirstName:
+                    <input type="text" name="firstName" onChange={handleInputChange} required />
+                </label>
+                <label>
+                    LastName:
+                    <input type="text" name="lastName" onChange={handleInputChange} required />
+                </label>
+                <label>
+                    Email:
+                    <input type="email" name="email" onChange={handleInputChange} required />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name="password" onChange={handleInputChange} required />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+            {userFound && <p>Пользователь добавлен в базу!</p>}
         </div>
-      
-    </>
-  );
-  
+    );
+};
+
 export default Reg;
